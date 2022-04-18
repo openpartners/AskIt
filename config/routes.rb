7009db28caa7b8212c20b03f901f1a745d938e65
Andrey Sevostyanov <andrzejsew@gmail.com>
2022-04-19 00:53:37 +0200

    step 3 part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get '/questions', to: 'questions#index'
-  
-  get '/questions/new', to: 'questions#new'
-  
-  post '/questions', to: 'questions#create'
+  resources :questions, only: %i[index new edit create update destroy show]
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # get '/questions/:id/edit', to: 'questions#edit'
+  # post '/questions', to: 'questions#create'
 
   root 'pages#index'
 end
