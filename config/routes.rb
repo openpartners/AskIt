0e0c5e242d8e8e765d01b33ba86141f54e8640d7
Andrey Sevostyanov <andrzejsew@gmail.com>
2022-04-21 00:00:10 +0200

    step 4 part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i[index new edit create update destroy show]
+  resources :questions
+  # resources :questions, only: %i[index new edit create update destroy show]
   # get '/questions', to: 'questions#index'
   # get '/questions/new', to: 'questions#new'
   # get '/questions/:id/edit', to: 'questions#edit'
