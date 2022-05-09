af2c2b46b7c1ed68dadddc6877d88b2d7e7e7817
Andrey Sevostyanov <andrzejsew@gmail.com>
2022-05-09 22:46:18 +0200

    step 9 log in log out dropdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :require_no_authentication
   def new
     @user = User.new
   end
   def create
      @user = User.new user_params
     if @user.save
-      session[:user_id] = @user.id
+      sign_in @user
       flash[:success] = "Welcome to the app, #{current_user.name_or_email}!"
       redirect_to root_path
     else
