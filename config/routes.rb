7857938d139f401e191774a48a77a52cefaf6e60
Andrey Sevostyanov <andrzejsew@gmail.com>
2022-05-10 17:45:48 +0200

    step 9 user update edit and menu profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
 
+  resources :users, only: %i[new create edit update]
 
-  resources :questions do 
-    resources :answers, except: %i[new show]  
+  resources :questions do
+    resources :answers, except: %i[new show]
   end
-
 
   root 'pages#index'
 end
 
 
-  # resources :questions, only: %i[index new edit create update destroy show]
-  # get '/questions', to: 'questions#index'
-  # get '/questions/new', to: 'questions#new'
-  # get '/questions/:id/edit', to: 'questions#edit'
-  # post '/questions', to: 'questions#create'
+# resources :questions, only: %i[index new edit create update destroy show]
+# get '/questions', to: 'questions#index'
+# get '/questions/new', to: 'questions#new'
+# get '/questions/:id/edit', to: 'questions#edit'
+# post '/questions', to: 'questions#create'
